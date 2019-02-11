210ff9e41e1dc16a7f71ac1b2de04a2a0ea88ebd
Allen F Partlow <allenfp@gmail.com>
2019-02-11 21:01:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,10 +13,14 @@ def line(katz_deli)
 
 end
 
+$line_number = 1 
+$line_array = []
 
-def take_a_number(katz_deli, new_customer)
-  katz_deli.push(new_customer)
-  puts "Welcome, #{new_customer}. You are number #{katz_deli.length} in line."
+def take_a_number()
+  # katz_deli.push(new_customer)
+  line_array.push($line_number)
+  puts "Welcome, #{$line_number}. You are number #{$line_array.length} in line."
+  $line_number += 1 
 end
 
 def now_serving(katz_deli)
